5e5f135f76f819d304d74f1fdcc46aae7cb2be0f
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-03-13 23:36:40 -0400

    added copyright notice

--- src/difAPI.cpp
+++ src/difAPI.cpp
@@ -1,5 +1,6 @@
 //------------------------------------------------------------------------------
 // Copyright (c) 2015 HiGuy Smith
+// Copyright (c) 2016 Jeff Hutchinson
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
